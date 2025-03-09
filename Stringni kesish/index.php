681f33fe98fa641124114674150b83245393437c
Asatbek2024 <qurbonovo2008@gmail.com>
2025-03-09 14:45:01 +0500

    Stringga oid misollar va tushunchalar

---
+++ Stringni kesish/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// Kesish
+
+
+// substr() funksiyasidan foydalanib o'zgarivchidagi strinbgin kesishimimz mumkin
+
+$x = "Hello world";
+
+
+echo substr($x , 6 , 5); /// $x bu o'zgaruvchini
+//     berib yuboryabmiz 6 esa chapdan o'nga qarab oltita harf tashlaydi va 5 esa 5 ta harfni o'qiydi 6 dan keyin
+
